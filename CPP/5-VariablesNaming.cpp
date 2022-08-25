ad31fa30c23140dee4246fb28c2b19541638a750
Amirhanna2001 <amirhanna2001@gmail.com>
2022-08-25 16:03:38 +0200

    Variables

---
+++ CPP/5-VariablesNaming.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Variables naming \n";
+    int number ;
+    int height;
+    int Long ;
+    // Variable naming rule :
+
+    //Must start with letter
+    // start with _ $
+
+    //should not start with number
+    int n = 20;
+    cout<<n;
+    //c++ is case sensitive
+    int numberOfDaysPerWeek;
+    int number_of_days_per_week;
+    return 0;
+}
---
+++ CPP/6-comment,bool and cin.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    /*
+        Data type : bool
+    */
+    
+    /*cout<<"Comment , boolean data type and cin \n"<<endl;
+    int w = 30; //This for width
+    bool b = true;
+    bool g = false;*/
+    
+    /*int w = 20;
+    int h = 10;
+    int area= w*h;
+    cout<<"Area is  = "<<area;*/
+    
+    /*int h , w;
+    cout<<"Enter height \n";
+    cin >> h;
+    cout<<"Enter width"<<endl;
+    cin >> w;
+    int area = h*w;
+    cout<<"Area is  = "<<area;*/
+    
+    int h,w;
+    cout<<"Enter h ,w \n";
+    cin>>h>>w;
+    cout<<"Area is = "<<h*w;
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
