b025a324c469d80088f1fbc1e5e8832b25ce9ca4
Amirhanna2001 <amirhanna2001@gmail.com>
2022-08-28 20:17:45 +0200

    excercise

---
+++ CPP/11-Exercises.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Exercises\n";
+   /* cout<<"1-Simple Calculator \n";
+
+
+
+        Algorithm:
+
+        1 - read number 1
+        2 - operator => + - / *
+        3 - read number 2
+
+        if op = +
+            n1 + n2
+        else if op = -
+
+
+    float n1,n2,result;
+    char op;
+    cout<<"Enter number 1"<<endl;
+    cin>>n1;
+    cout<<"Enter operator \n";
+    cin>>op;
+    cout<<"Enter number 2\n";
+    cin>>n2;
+
+
+    if(op == '+')
+        result = n1+n2;
+    else if(op == '-')
+        result = n1-n2;
+    else if(op == '*')
+        result = n1*n2;
+    else if(op == '/')
+        result = n1/n2;
+
+    cout<<"Result = "<<result;*/
+
+
+
+
+
+    //write a program print if year is a leap year or not
+
+    /*
+        by knowing that leap year
+        (not divisible by 100 and  divisible by 4) or
+        (divisible by 400)
+
+
+            Algorithm :
+            read year
+            if
+
+        int y ;
+        cout<<"Enter year\n";
+        cin>>y;
+        //n1 % n2 == 0
+        if(y %100 != 0 && y % 4 == 0)
+            cout<<"Is leap year\n";
+        else if(y % 400 == 0)
+            cout<<"Is leap year\n";
+        else
+            cout<<"Is not leap year\n";*/
+
+
+    //write a program to print if the number is odd or even
+
+
+    // read n
+    // check if n % 2 == 0
+
+    int num ;
+    cout<<"Enter number \n";
+    cin>>num;
+    if(num % 2 == 0)
+            cout<<"Even\n";
+    else
+        cout<<"Odd\n";
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
